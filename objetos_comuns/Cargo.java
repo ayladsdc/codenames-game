e2ced143b9756e59310693447c7b06670307cf9c
bianatriz <ana.beatriz.rasec@gmail.com>
2026-09-23 00:44:11 -0300

    add cargo

---
+++ objetos_comuns/Cargo.java
@@ -0,0 +1,18 @@
+package objetos_comuns;
+
+public enum Cargo {
+    VERMELHO_MESTREESPIAO,
+    VERMELHO_AGENTE,
+    AZUL_MESTREESPIAO,
+    AZUL_AGENTE;
+
+    // A qual time este papel pertence
+    public CorCarta time() {
+        return (this == VERMELHO_MESTREESPIAO || this == VERMELHO_AGENTE) ? CorCarta.VERMELHO : CorCarta.AZUL;
+    }
+
+    public boolean eMestreEspiao() {
+        return this == VERMELHO_MESTREESPIAO || this == AZUL_MESTREESPIAO;
+    }
+}
+
