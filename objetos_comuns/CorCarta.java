c797b39c9e17bdec802751b159fedf903611697c
bianatriz <ana.beatriz.rasec@gmail.com>
2026-09-23 00:15:11 -0300

    Arquitetura + criação de objetos

---
+++ objetos_comuns/CorCarta.java
@@ -0,0 +1,8 @@
+package objetos_comuns;
+
+public enum CorCarta {
+    VERMELHO,
+    AZUL,
+    NEUTRA,
+    ASSASSINO
+}
---
+++ objetos_comuns/Tabuleiro.java
@@ -0,0 +1,19 @@
+package objetos_comuns;
+
+public class Tabuleiro {
+    public static int tamanhoTabuleiro;
+    private static int cartasTimeInicio = 9;
+    private static int cartasOutroTime = 8;
+    private static int cartasNeutras = 7;
+    private static int cartasAssassino = 1;
+
+    private static String[] possiveisPalavras = {};
+
+    //escolhe palavras
+    //distribui palavras aleatoriamente para os dois times, 
+    //conforme os numeros das vars
+    
+    //funções de exibição por cargo
+
+    //mostrar quantas cartas faltam para cada time
+}
